2374ed794dd7f825fb4cec340bea663bda6eeef5
amol <amoltribhuwan05@gmail.com>
2024-05-27 15:24:46 +0530

    mycommit for push Aquala cms application

--- scripts/setupTestEnv.js
+++ scripts/setupTestEnv.js
@@ -9,7 +9,7 @@ const {fs} = require('aql-utils');
 
         const envJsonContent = {
             test : {
-                db      : 'mongodb://10.0.157.5:27017/test',
+                db      : 'mongodb://10.0.159.76:27017/test',
                 devMode : {
                     mailTo  : 'testaquila@yopmail.com',
                     compile : false,
